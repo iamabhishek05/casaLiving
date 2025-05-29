74b29d551fe71933199debb7b194aef87f9cbd79
vivek singh <vivek@sapioglobal.com>
2025-05-30 00:20:47 +0530

    new Ui changes

--- casalivingapp/models.py
+++ casalivingapp/models.py
@@ -25,6 +25,7 @@ class HouseImage(models.Model):
 # Tags for amenities (e.g., WiFi, AC, Parking, etc.)
 class AmenityTag(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='amenities/', default='')
 
     def __str__(self):
         return self.name
